1b8d4d379ee149f5c2c1e423bc1b8e82e04dba61
DanielW705 <63887171+DanielW705@users.noreply.github.com>
2021-05-23 12:22:38 -0500

    Add files via upload

---
+++ TetrisCsharp2/Models/TetriminoJ.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TetrisCsharp2.Models
+{
+    class TetriminoJ : Tetrimino
+    {
+        public TetriminoJ(TypesOfTetrimino _tipos = TypesOfTetrimino.TetriminoJ) : base(_tipos)
+        {
+            this.figura = this.constuirTetrimino();
+        }
+        public override Label[] constuirTetrimino()
+        {
+            Label[] tetrimino = new Label[4];
+            for (int i = 0; i < tetrimino.Length; i++)
+            {
+                Label pieza = new Label();
+                pieza.Size = new Size(50, 50);
+                pieza.Location = (i == 3 ? new Point(150, 0 + ((i - 1) * 50)) : new Point(200, 0 + (i * 50)));
+                pieza.BackColor = GetColor();
+                pieza.Enabled = false;
+                this.posicion[i] = (i == 3 ? (3, 0 + ((i - 1) * 1)) : (4, 0 + (i * 1)));
+                tetrimino[i] = pieza;
+            }
+            return tetrimino;
+        }
+    }
+}
